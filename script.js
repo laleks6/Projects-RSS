f4cc56dfe68beeb689f172ba37f2a94f2bc9af80
laleks6 <lomanovsaha98@gmail.com>
2023-05-13 09:31:29 +0300

    refactor: move all files to a folder shelter

--- script.js
+++
@@ -1,6 +0,0 @@
-console.log("Моя оценка 100 баллов\nЕсли есть недочеты, то пишите в дискорд (@laleks#0503), либо в проверочном чате RS")
-const burgerIcon = document.querySelector(".burger__icon");
-
-burgerIcon.addEventListener('click', () => {
-    burgerIcon.classList.toggle("burger__acrive")
-})
