644798d853b4f96bde548c76a20fbcfd692c97fa
laleks6 <lomanovsaha98@gmail.com>
2023-05-17 22:50:33 +0300

    feat: add color code

--- minesweeper/script.js
+++ minesweeper/script.js
@@ -47,6 +47,30 @@ body.prepend(header);
 const playingField = document.querySelector('.minesweeper-block__playing-field');
 
 const quantity = 10;
+let countSecond = 0;
+let interval;
+
+function timeOut() {
+  countSecond++;
+  console.log(countSecond);
+  informationTime.textContent = countSecond;
+}
+function timerOff(interval) {
+  interval = clearInterval(interval);
+  informationTime.classList.toggle('active--timer');
+  return interval;
+}
+function time(el) {
+  if (!informationTime.classList.contains('active--timer')) {
+    interval = setInterval(timeOut, 1000);
+    informationTime.classList.toggle('active--timer');
+  }
+  if (el.target.classList.contains('bomb')) {
+    timerOff(interval);
+  }
+}
+
+// создание кнопок
 const creatBtn = (quantity) => {
   let x = 0;
   let y = 0;
@@ -55,7 +79,7 @@ const creatBtn = (quantity) => {
     playingFieldBlock.classList.add('grid-btn-easy');
     for (let i = 0; i < quantity * quantity; i += 1) {
       const btn = document.createElement('button');
-      btn.className = 'btn zero';
+      btn.className = 'btn number';
       btn.id = i;
       if (x >= quantity) {
         x = 0;
@@ -70,21 +94,47 @@ const creatBtn = (quantity) => {
 };
 
 creatBtn(quantity);
+// рандомные цыфры запушенные в массив
 
-function buttonAssembly(quantity, el) {
+function rundomNumver(quantity, array, el) {
+  for (let i = 0; i < quantity * quantity; i += 1) {
+    if (+el.id !== +i) {
+      array.push(i);
+    }
+  }
+  for (let i = array.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+// добавленияе класса для рандомных значений в массиве
+function addClassBomb(array, arrBtn) {
+  for (let i = 0; i < array.slice(0, 10).length; i += 1) {
+    const btn = arrBtn[array[i]];
+    btn.classList.add('bomb');
+    btn.classList.remove('number');
+  }
+}
+// делаем проверка начала игры, если это новая игра то добавляем рандомные бомбы
+function startGame(arrBtn, array, el) {
   if (!playingField.classList.contains('active-game')) {
     playingField.classList.toggle('active-game');
-    const arrBtn = document.getElementsByClassName('btn');
-    const array = [];
     rundomNumver(quantity, array, el);
     addClassBomb(array, arrBtn);
   }
-  el.textContent = el.classList.contains('bomb') ? 'X' : bombCount(el, quantity);
 }
-function bombCount(el, quantity) {
+// функция нахождение бомбы
+function findingBomb(x, y, el, quantity) {
+  const arrBtn = document.getElementsByClassName('btn');
+  const arr = [...arrBtn][+`${y}${x}`];
+  if (x >= 0 && y >= 0 && y < quantity && x < quantity) {
+    if (arr.classList.contains('bomb')) { return true; }
+  }
+}
+
+// подсчет бомб вокруг клетки
+function bombCount(el, quantity, x, y) {
   let count = 0;
-  const { x } = el.dataset;
-  const { y } = el.dataset;
   console.log(el);
   console.log(`${x} - x`);
   console.log(`${y} - y`);
@@ -98,44 +148,48 @@ function bombCount(el, quantity) {
   }
   return count;
 }
-function findingBomb(x, y, el, quantity) {
+
+// функция сборщик всего функционала по кноакам
+function buttonAssembly(quantity, el, x, y) {
   const arrBtn = document.getElementsByClassName('btn');
-  const arr = [...arrBtn][+`${y}${x}`];
-  console.log(x);
-  console.log(y);
-  console.log(arr);
-  console.log(quantity);
-  console.log([...arrBtn]);
+  const array = [];
+  el = arrBtn[+`${y}${x}`];
   if (x >= 0 && y >= 0 && y < quantity && x < quantity) {
-    if (arr.classList.contains('bomb')) { return true; }
-  }
-}
-function rundomNumver(quantity, array, el) {
-  for (let i = 0; i < quantity * quantity; i += 1) {
-    if (+el.id !== +i) {
-      array.push(i);
+    if (!el.classList.contains('active-btn')) {
+      startGame(arrBtn, array, el);
+      el.classList.add('active-btn', 'btn--unlock');
+      el.textContent = ' ';
+      if (el.classList.contains('bomb')) {
+        el.textContent = 'X';
+      } else {
+        const count = bombCount(el, quantity, x, y);
+        if (count === 0) {
+          for (let i = -1; i <= 1; i += 1) {
+            for (let k = -1; k <= 1; k += 1) {
+              buttonAssembly(quantity, el, +x + i, +y + k);
+            }
+          }
+        } else {
+          el.textContent = count;
+          // добавление нужного класса цвета для цифры
+          el.classList.add(`btn--number-color-${count}`);
+          return console.log('stop');
+        }
+      }
     }
-  }
-  for (let i = array.length - 1; i > 0; i -= 1) {
-    const j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
-function addClassBomb(array, arrBtn) {
-  for (let i = 0; i < array.slice(0, 10).length; i += 1) {
-    const btn = arrBtn[array[i]];
-    btn.classList.add('bomb');
-    btn.classList.remove('zero');
   }
 }
 
 const clickbtn = (el) => {
   if (el.target.classList.contains('btn')) {
-    buttonAssembly(quantity, el.target);
+    time(el);
+    const { x } = el.target.dataset;
+    const { y } = el.target.dataset;
+    buttonAssembly(quantity, el.target, x, y);
     console.log(el);
     console.log(el.target);
     console.log(el.target.classList);
-    console.log(`${el.x}--${el.y}`);
+    console.log(`${el.target.dataset.y}--${el.target.dataset.x}`);
   }
 };
 playingField.addEventListener('click', clickbtn);
