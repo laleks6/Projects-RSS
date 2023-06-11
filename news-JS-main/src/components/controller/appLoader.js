98e9a0744f85cde12bf6fcd85a4087e50da4ce2c
laleks6 <lomanovsaha98@gmail.com>
2023-06-11 10:02:38 +0300

    refactor: add API key

--- news-JS-main/src/components/controller/appLoader.js
+++ news-JS-main/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'fbd0bd9b9ca1431bb0cda8d17722bcb7', // получите свой ключ https://newsapi.org/
         });
     }
 }
