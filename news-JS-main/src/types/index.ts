6e7f9ee7e6dde07da2d0ee624d3bebf6d28e6002
laleks6 <lomanovsaha98@gmail.com>
2023-06-14 22:03:21 +0300

    feat: add types interface

--- news-JS-main/src/types/index.ts
+++ news-JS-main/src/types/index.ts
@@ -1,3 +1,18 @@
-export interface Inews {
-    
+export interface INewsAllProps {
+    status: string;
+    totalResult: number;
+    articles?: INewsProps[];
+}
+export interface INewsProps {
+    source: {
+        id: string;
+        name: string;
+    };
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
 }
