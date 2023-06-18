61d8107b69b80f6255ce429d619c572c27d023d4
laleks6 <lomanovsaha98@gmail.com>
2023-06-18 13:51:51 +0300

    refactor: change appView

--- news-JS-main/src/components/view/appView.ts
+++ news-JS-main/src/components/view/appView.ts
@@ -1,18 +1,20 @@
 import News from './news/news';
 import Sources from './sources/sources';
-
+import { INewsAllProps, INewsProps } from 'src/types/index';
 export class AppView {
+    news: News;
+    sources: Sources;
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: INewsAllProps): void {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: INewsProps): void {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
