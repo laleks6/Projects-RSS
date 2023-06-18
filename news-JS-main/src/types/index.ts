ee6e5e99bc3ef9c0f3cbfcbb8e80dad2bc583c5c
laleks6 <lomanovsaha98@gmail.com>
2023-06-18 10:28:14 +0300

    refactor: change interface

--- news-JS-main/src/types/index.ts
+++ news-JS-main/src/types/index.ts
@@ -1,13 +1,10 @@
 export interface INewsAllProps {
     status: string;
     totalResult: number;
-    articles?: INewsProps[];
+    articles?: Array<INewsProps>;
 }
 export interface INewsProps {
-    source: {
-        id: string;
-        name: string;
-    };
+    sources?: Isources[];
     author: string;
     title: string;
     description: string;
@@ -15,4 +12,10 @@ export interface INewsProps {
     urlToImage: string;
     publishedAt: string;
     content: string;
+}
+export interface Isources {
+    sources: {
+        id?: string;
+        name?: string;
+    };
 }
