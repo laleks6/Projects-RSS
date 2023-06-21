48615d5f923322e5bcba9b1e49aaac287f3695c0
laleks6 <lomanovsaha98@gmail.com>
2023-06-21 22:59:59 +0300

    refactor: finished change app

---
+++ news-JS-main/src/components/app/app.ts
@@ -0,0 +1,23 @@
+import { INewsAllProps, INewsProps } from '../../types/index';
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+
+class App {
+    controller: AppController;
+    view: AppView;
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    public start(): void {
+        const sources: HTMLElement = document.querySelector('.sources') as HTMLElement;
+
+        sources.addEventListener('click', (e: Event): void =>
+            this.controller.getNews(e, (data: INewsAllProps) => this.view.drawNews(data))
+        );
+        this.controller.getSources((data: INewsProps) => this.view.drawSources(data));
+    }
+}
+
+export default App;
