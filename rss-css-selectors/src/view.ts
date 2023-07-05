d4cdb6c3ef0b1da92b887ea76f47eb5b3755a87a
laleks6 <lomanovsaha98@gmail.com>
2023-07-05 22:33:21 +0300

    feat: add REAL view

---
+++ rss-css-selectors/src/view.ts
@@ -0,0 +1,30 @@
+import { task } from "./task"
+import { EventEmitter } from './emitter'
+
+export class View {
+    eventEmitter: EventEmitter = new EventEmitter();
+    viewerCode: HTMLCollectionOf<Element> = document.getElementsByClassName('code-highlighting')
+    constructor(){
+        
+    }
+    addTags = (value: string): void => {
+        const table = document.querySelector('.table') as HTMLDivElement
+        const viewer = document.querySelector('.main-viewer') as HTMLDivElement
+
+        const plate: Element[] = [...document.getElementsByTagName('plate')]
+        const code: string | undefined = task[value].view; 
+        viewer.innerHTML= `<div>&lt;div class="table"&gt;</div>${code}<div>&lt;/div&gt;</div>`
+        // viewer.innerHTML = '&lt;palte/&gt;&lt;palte/&gt;'
+            
+
+    }
+    addHighlighting = ():void => {
+        for (let i = 0; i < this.viewerCode.length; i++) {
+            console.log(this.viewerCode[i])
+            this.viewerCode[i].addEventListener('mouseover', (val) => {
+                console.log(val)
+             })
+        }
+
+    }
+}
