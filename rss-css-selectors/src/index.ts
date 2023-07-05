1a0bb5f595cc1ac80f5618369df51937ef42ced1
laleks6 <lomanovsaha98@gmail.com>
2023-07-05 22:38:06 +0300

    refactor: change style abd index

--- rss-css-selectors/src/index.ts
+++ rss-css-selectors/src/index.ts
@@ -1,6 +1,29 @@
 import './style.css'
-import { title } from './rtitle'
+import { Table } from './table'
+import { Editor } from './editor'
+import { Level } from './level'
+import { View } from './view'
+import { EventEmitter } from './emitter'
+const table = new Table()
+const view = new View()
+const level = new Level('active')
+const editor = new Editor()
 
-const ret = new title()
-ret.hi()
-console.log();
+view.addHighlighting()
+
+level.eventLevels()
+const eventEmitter = new EventEmitter()
+eventEmitter.subscribe('start', () => {
+    let taskVal = `task${level.returnIdLevels()}`
+    table.tableFilling(taskVal)
+    view.addTags(taskVal)
+    editor.checkInput(taskVal)
+})
+eventEmitter.subscribe('view', () => {
+
+})
+
+
+eventEmitter.dispath('start')
+
+    
